c3f7536a31ec039cec9ed070b437882704898116
zhuangwu <562669088@qq.com>
2017-05-03 22:43:39 +0800

    修改摄像头预览布局

--- app/src/main/java/com/kuan/retrofitrxjavacamrea/FullscreenActivity.java
+++ app/src/main/java/com/kuan/retrofitrxjavacamrea/FullscreenActivity.java
@@ -8,7 +8,6 @@ import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import java.io.File;
