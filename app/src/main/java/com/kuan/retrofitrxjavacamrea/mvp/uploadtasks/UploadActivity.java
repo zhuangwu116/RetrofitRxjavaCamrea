080a53bdd0f4acb9c6ebed0188a46de4931ee469
zhuangwu <zhuangwu@zkhc.org>
2017-05-03 20:25:44 +0800

    搭建mvp框架

---
+++ app/src/main/java/com/kuan/retrofitrxjavacamrea/mvp/uploadtasks/UploadActivity.java
@@ -0,0 +1,27 @@
+package com.kuan.retrofitrxjavacamrea.mvp.uploadtasks;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.kuan.retrofitrxjavacamrea.R;
+
+public class UploadActivity extends AppCompatActivity implements UploadTasksContract.View{
+    private UploadTasksContract.Presenter mPresenter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uploadtasks);
+
+    }
+
+    @Override
+    public void setPresenter(UploadTasksContract.Presenter presenter) {
+        if(presenter!=null){
+            mPresenter=presenter;
+        }
+    }
+}
